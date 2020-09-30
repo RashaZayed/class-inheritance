297ff7fba5b97fa4d109ee09eedf5377a7237637
Rasha Zayed <zayed.rasha@outlook.com>
2020-09-30 12:40:11 -0700

    parent and chlid classes in the same file

--- inheritance.js
+++ inheritance.js
@@ -22,3 +22,24 @@ const ninja1 = new Ninja("Hyuabusa", 20);
 ninja1.sayName();
 ninja1.drinkSake();
 ninja1.showStates();
+
+class Sensi extends Ninja {
+  constructor(wisdom) {
+    super(name, 200, wisdom);
+    this.wisdom = 10;
+    this.speed = 10;
+    this.strength = 10;
+  }
+  speakWisdom() {
+    console.log("Be kind to all");
+    super.drinkSake();
+  }
+}
+
+const superSensi = new Sensi();
+superSensi.name = "Master Splinter";
+superSensi.speakWisdom();
+console.log(superSensi.wisdom);
+superSensi.showStates();
+console.log(superSensi.health);
+superSensi.sayName();
